d4758a8d0e10a58226b0fa6d554eaf40c972a701
vaarun18 <varunsadananda108@gmail.com>
2023-05-27 18:08:32 +0530

    modified:   client/src/components/Home/FAQ.jsx

--- client/src/components/Home/FAQ.jsx
+++ client/src/components/Home/FAQ.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { faq } from "../../util/data";
+import { FiPlus } from "react-icons/fi";
+import { AiOutlineMinus } from "react-icons/ai";
+import { calcLength } from "framer-motion";
+
 const FAQ = () => {
+  const [isClicked, setIsClicked] = useState(null);
+
+  const handleClick = (id) => {
+    if (id === isClicked) {
+      return setIsClicked(null);
+    }
+    setIsClicked(id);
+    console.log(isClicked);
+    console.log(id === isClicked);
+  };
+
+  const open = "block ";
+  const close = "hidden";
+
   return (
-    <div>
+    <div className="rounded-div">
       {faq.map((item) => {
         const { id, question, answer } = item;
-        return <div key={id}></div>;
+        return (
+          <div className="border my-6">
+            <div className="flex justify-between">
+              <h1>{question}</h1>
+              <button
+                onClick={() => {
+                  handleClick(id);
+                }}
+              >
+                <FiPlus key={id} />
+              </button>
+            </div>
+            <p className={isClicked === id ? open : close}>{answer}</p>
+          </div>
+        );
       })}
     </div>
   );
