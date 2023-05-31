a5865f77e17bbf7a07473ca50061f30d33ae3b33
vaarun18 <varunsadananda108@gmail.com>
2023-05-31 21:25:38 +0530

    modified:   client/src/pages/Dashboard/AllCourses.jsx

--- client/src/pages/Dashboard/AllCourses.jsx
+++ client/src/pages/Dashboard/AllCourses.jsx
@@ -1,42 +1,76 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deleteCourse, getAllCourse } from "../../redux/slices/courseSlice";
 import { Link } from "react-router-dom";
+import Loader from "../../components/Loader";
 const AllCourses = () => {
   const [courses, setCourses] = useState([]);
   const dispatch = useDispatch();
 
+  const { isLoading } = useSelector((state) => state.course);
+
   const getAllCourses = async () => {
     const response = await dispatch(getAllCourse({}));
-    console.log(response.payload);
+    console.log(response.payload.courses);
     setCourses(response.payload.courses);
   };
 
   const deleteCourses = async (id) => {
     console.log(id);
     dispatch(deleteCourse(id));
+    getAllCourse();
   };
 
   useEffect(() => {
     getAllCourses();
   }, []);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
-    <div>
+    <div className="px-20 md:grid grid-cols-2 gap-8 lg:grid-cols-3">
       {courses &&
         courses.map((course) => {
-          const { _id, title, description, createdBy, numberOfVideos } = course;
+          const {
+            _id,
+            title,
+            createdBy,
+            numberOfVideos,
+            category,
+            poster: { url },
+            numOfVideos,
+            lectures,
+          } = course;
           return (
-            <div key={_id} className="bg-green-500 my-5">
-              <h1>{title}</h1>
-              <h1>{description}</h1>
-              <h1>{createdBy}</h1>
-              <h1>{numberOfVideos}</h1>
-              <button onClick={() => deleteCourses(_id)} className="bg-red-500">
+            <div key={_id} className="shadow-2xl shadow-gray-900 p-4 my-8">
+              <div className="grid place-items-center">
+                <img className="h-40" src={url} alt="" />
+              </div>
+              <h1 className="font-bold text-sm my-2 md:text-xl">{title}</h1>
+              <h1 className=" my-2">
+                <span className="font-bold ">Creator</span> {createdBy}
+              </h1>
+
+              <h1 className=" text-sm my-2 lg:text-xl">
+                <span className="font-bold">Category</span> {category}
+              </h1>
+              <h1 className="text-sm  my-2 lg:text-xl">
+                <span className="font-bold">Total Videos </span>
+                {numberOfVideos}
+              </h1>
+              <h1 className="font-bold text-sm my-2 lg:text-xl">
+                {lectures?.length}
+              </h1>
+              <button
+                onClick={() => deleteCourses(_id)}
+                className="text-sm button-danger "
+              >
                 Delete Course
               </button>
               <Link to={`/admin/allCourses/${_id}`}>
-                <button>Add Lectures</button>
+                <button className="text-sm button-input">Add Lectures</button>
               </Link>
             </div>
           );
